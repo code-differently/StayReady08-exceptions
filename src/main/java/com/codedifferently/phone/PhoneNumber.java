51ec28ec475e3d9d5eb075a82e7c8edb36316a8f
Giovane Barnes <gjbarnes19@students.desu.edu@Giovanes-MacBook-Pro.local>
2020-07-23 09:02:42 -0400

    Finished Lab

--- src/main/java/com/codedifferently/phone/PhoneNumber.java
+++ src/main/java/com/codedifferently/phone/PhoneNumber.java
@@ -7,35 +7,30 @@ import com.codedifferently.exceptions.InvalidPhoneNumberFormatException;
  */
 public final class PhoneNumber {
     private final String phoneNumberString;
-
     // default constructor is uncallable
     private PhoneNumber() throws InvalidPhoneNumberFormatException {
         this(null);
     }
-
     // non-default constructor is package-protected
     protected PhoneNumber(String phoneNumber) throws InvalidPhoneNumberFormatException {
         //validate phone number with format `(###)-###-####`
         if (!phoneNumber.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")) {
-            throw new InvalidPhoneNumberFormatException();
+            throw new InvalidPhoneNumberFormatException(phoneNumber + "Is not in the correct format");
         }
         this.phoneNumberString = phoneNumber;
     }
-
     public String getAreaCode() {
         return toString().substring(1, 4);
     }
-
     public String getCentralOfficeCode() {
         return toString().substring(6, 9);
     }
-
     public String getPhoneLineCode() {
         return toString().substring(10, 14);
-}
-
+    }
     @Override
     public String toString() {
         return phoneNumberString;
     }
 }
+

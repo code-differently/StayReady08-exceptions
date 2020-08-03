cdf817ee802be79cc2cf767edd2ee1d7ca9c5009
Madlyn Espinoza <37963506+madlynjoa1@users.noreply.github.com>
2020-08-03 17:04:59 -0400

    Update PhoneNumberFactoryTest.java

--- src/test/java/com/codedifferently/PhoneNumberFactoryTest.java
+++ src/test/java/com/codedifferently/PhoneNumberFactoryTest.java
@@ -22,69 +22,59 @@ public class PhoneNumberFactoryTest {
 
     @Test
     public void testCreatePhoneNumberSafely() {
-        // : Given
+       
         int areaCode = 0;
         int centralOfficeCode = 0;
         int phoneLineCode = 0;
 
-        // : When
         PhoneNumber phoneNumber = PhoneNumberFactory.createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
-
-        // : Then
+        
         Assert.assertEquals(null, phoneNumber);
     }
 
     @Test
     public void testGetAreaCode() {
-        // : Given
+        
         Integer areaCode = 302;
         int centralOfficeCode = 312;
         int phoneLineCode = 5555;
-
-        // : When
+        
         PhoneNumber phoneNumber = PhoneNumberFactory.createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
 
-        // : Then
         Assert.assertEquals(phoneNumber.getAreaCode(), areaCode.toString());
     }
 
     @Test
     public void testGetCentralOfficeCode() {
-        // : Given
+
         int areaCode = 302;
         Integer centralOfficeCode = 312;
         int phoneLineCode = 5555;
 
-        // : When
         PhoneNumber phoneNumber = PhoneNumberFactory.createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
-
-        // : Then
+        
         Assert.assertEquals(phoneNumber.getCentralOfficeCode(), centralOfficeCode.toString());
     }
 
 
     @Test
     public void testPhoneLineCode() {
-        // : Given
+  
         int areaCode = 302;
         int centralOfficeCode = 312;
         Integer phoneLineCode = 5555;
 
-        // : When
         PhoneNumber phoneNumber = PhoneNumberFactory.createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
 
-        // : Then
         Assert.assertEquals(phoneNumber.getPhoneLineCode(), phoneLineCode.toString());
     }
 
     @Test
     public void testCreateRandomPhoneNumber() {
         for (int i = 0; i < 999; i++) {
-            // : Given
-            // : When
+
             PhoneNumber phoneNumber = PhoneNumberFactory.createRandomPhoneNumber();
 
-            // : Then
             Assert.assertTrue(phoneNumber != null);
         }
     }
